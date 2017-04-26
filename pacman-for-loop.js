98e03a850b124f7eb7410522a4a75aaa99f8a397
EDWIN AU <edwinau@rogers.com>
2017-04-26 09:47:07 -0400

    JS for loop

---
+++ pacman-for-loop.js
@@ -0,0 +1,8 @@
+var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
+
+for (var index = 0; index < ghosts.length; index ++) {
+    console.log('Pac-Man eats ' + ghosts[index]);
+}
+
+
+console.log(ghosts.length)
